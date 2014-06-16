f9fae4bb0fc25d53bfa8435606c78c9c3e0cd9c0
Shane L Hubler <Shane.L.Hubler@gmail.com>
2014-06-16 17:56:52 -0500

    Completed assignment
    Made necessary changes to make function work. I also added documenting
    comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This module creates a way to cache inverted matrices
+## It uses two functions makeCacheMatrix and cacheSolve.
+## Example of use:
+##      mm = matrix(rnorm(n=10000),nrow=100)
+##      mc = makeCacheMatrix(mm)
+##      result1 = cacheSolve(mc)
+##      result2 = cacheSolve(mc) # second time is faster
 
-## Write a short comment describing this function
+## Creates list of functions that are specific to the 
+##      input matrix
+## Names of functions in list:
+##      set -- sets the original matrix
+##      get -- gets the original matrix
+##      setsolve -- sets result of "solving"
+##      getsolve -- gets result of solving
+##              (or NULL if not set yet)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## "Solves" matrix if not done yet.
+## Otherwise retrieves original result
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
